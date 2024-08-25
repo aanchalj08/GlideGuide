3682bd9aa73b082deccae25df8f5480cad48118a
aanchalj08 <120584888+aanchalj08@users.noreply.github.com>
2024-08-25 20:29:08 +0530

    Added project files

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db/connect");
+const mainRouter = require("./routes/user");
+const { forgotPassword, resetPassword } = require("./controllers/user");
+const authMiddleware = require("./middleware/auth");
+const {
+  saveItinerary,
+  getUserItineraries,
+  getItineraryById,
+} = require("./controllers/itinerary");
+
+const app = express();
+const port = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: "GET,POST",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
+
+app.post("/api/v1/forgot-password", forgotPassword);
+app.post("/api/v1/reset-password/:resetToken", resetPassword);
+app.post("/api/v1/itinerary/save-itinerary", authMiddleware, saveItinerary);
+app.get(
+  "/api/v1/itinerary/user-itineraries",
+  authMiddleware,
+  getUserItineraries
+);
+app.get("/api/v1//itinerary/:id", authMiddleware, getItineraryById);
+
+app.use("/api/v1", mainRouter);
+
+const start = async () => {
+  try {
+    await connectDB(MONGODB_URI);
+    app.listen(port, () => {});
+  } catch (error) {}
+};
+
+start();
