4c544ac73848369f12ffcb1f39e3fb43f897d754
aanchalj08 <120584888+aanchalj08@users.noreply.github.com>
2024-08-25 21:03:11 +0530

    changed the main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,17 @@ import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
 import "./responsive.css";
 
+const loadGoogleMapsScript = () => {
+  const apiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+  const script = document.createElement("script");
+  script.src = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`;
+  script.async = true;
+  script.defer = true;
+  document.body.appendChild(script);
+};
+
+loadGoogleMapsScript();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
